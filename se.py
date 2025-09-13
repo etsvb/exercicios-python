94bf57d5c6b4c6e021c9e1431aa054c7111f12d8
Ericha Barbosa <erichataina02@gmail.com>
2025-09-12 21:44:12 -0300

    Add files via upload

---
+++ se.py
@@ -0,0 +1,7 @@
+# esta = str(input("está sol?: "))
+# dinheiro = str(input("está com dinheiro?: "))
+# if esta == "sim" and dinheiro == "sim":
+#     print("então vamos a praia")
+# else:
+#     print("não, vamos ver netflix")
+
