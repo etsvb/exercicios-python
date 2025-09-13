a5b2ecbfa5f5466c3da6642066c35eab805aece7
Ericha Barbosa <erichataina02@gmail.com>
2025-09-12 21:20:47 -0300

    Add files via upload

---
+++ comandoscondicionais.py
@@ -0,0 +1,34 @@
+# a = int(input("Digite um número inteiro: "))
+# impar = (a % 2) == 1
+# if impar:
+#     print("Número ímpar")
+# print("Fim do programa")
+
+# a = int(input("Digite um número inteiro: "))
+# if (a % 2) == 0:
+#     print("Número par")
+# if (a % 2) == 1:
+#     print("Número impar")
+
+# print("Fim do programa")
+
+# a = float(input("Digite o primeiro número: "))
+# b = float(input("Digite o segundo número: "))
+
+# if a > b:
+#     print("O maior número é", a)
+# else:
+#     print("O maior número é", b)
+
+# a = float(input("Digite o primeiro número: "))
+# b = float(input("Digite o segundo número: "))
+
+# if a == b:
+#     print("Os dois número são iguais")
+# else:
+#     print("Os numeros sao diferentes")
+# if a > b:
+#     print("O número é o primeiro")
+# else:
+#     print("O maior número é o segundo")
+
