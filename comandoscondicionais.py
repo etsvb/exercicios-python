a4f7a03645eb34af87440366673ec5d70ee04dc5
Ericha Barbosa <erichataina02@gmail.com>
2025-09-19 19:42:34 -0300

    Update comandoscondicionais.py

--- comandoscondicionais.py
+++ comandoscondicionais.py
@@ -1,22 +1,22 @@
-# a = int(input("Digite um número inteiro: "))
-# impar = (a % 2) == 1
-# if impar:
-#     print("Número ímpar")
-# print("Fim do programa")
+a = int(input("Digite um número inteiro: "))
+impar = (a % 2) == 1
+if impar:
+     print("Número ímpar")
+ print("Fim do programa")
 
-# a = int(input("Digite um número inteiro: "))
-# if (a % 2) == 0:
-#     print("Número par")
-# if (a % 2) == 1:
-#     print("Número impar")
+ a = int(input("Digite um número inteiro: "))
+ if (a % 2) == 0:
+     print("Número par")
+ if (a % 2) == 1:
+     print("Número impar")
 
-# print("Fim do programa")
+ print("Fim do programa")
 
-# a = float(input("Digite o primeiro número: "))
-# b = float(input("Digite o segundo número: "))
+ a = float(input("Digite o primeiro número: "))
+ b = float(input("Digite o segundo número: "))
 
-# if a > b:
-#     print("O maior número é", a)
+ if a > b:
+     print("O maior número é", a)
 # else:
 #     print("O maior número é", b)
 
@@ -31,4 +31,5 @@
 #     print("O número é o primeiro")
 # else:
 #     print("O maior número é o segundo")
+
 
