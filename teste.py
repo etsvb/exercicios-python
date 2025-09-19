4a12e2e9b8ebca1d43b10c66cef234e2513cc5f3
Ericha Barbosa <erichataina02@gmail.com>
2025-09-19 19:59:52 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,20 +1,21 @@
-# numero_1 = 5
-# numero_1 +=2
-# print(numero_1)
+ numero_1 = 5
+ numero_1 +=2
+ print(numero_1)
 
-# numero_1 = 5
-# numero_1 -=2
-# print(numero_1)
+ numero_1 = 5
+ numero_1 -=2
+ print(numero_1)
 
 soma_1 = 7 + 8
 soma_2 = 10 + 5
 
-# print(soma_1)
-# print(soma_2)
+ print(soma_1)
+ print(soma_2)
 
-# print(soma_1, soma_2)
+ print(soma_1, soma_2)
 
 if soma_1 == soma_2:
     print("Os resultados são iguais")
 else:
+
     print("Os resultados são diferentes")
