ebbab3d6a841e2ae3bcb494ea9a62e146b89db10
Ericha Barbosa <erichataina02@gmail.com>
2025-09-19 20:00:06 -0300

    Update se.py

--- se.py
+++ se.py
@@ -1,7 +1,8 @@
-# esta = str(input("está sol?: "))
-# dinheiro = str(input("está com dinheiro?: "))
-# if esta == "sim" and dinheiro == "sim":
-#     print("então vamos a praia")
-# else:
-#     print("não, vamos ver netflix")
+ esta = str(input("está sol?: "))
+ dinheiro = str(input("está com dinheiro?: "))
+ if esta == "sim" and dinheiro == "sim":
+     print("então vamos a praia")
+ else:
+     print("não, vamos ver netflix")
+
 
